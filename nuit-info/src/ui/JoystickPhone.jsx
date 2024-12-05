a681ab7ea663f31d01f44562566c84ea165640fd
kiliansoudier <soudierkilian@outlook.com>
2024-12-05 21:26:31 +0100

    feat(joystick, css): Joystick done without logic

--- nuit-info/src/ui/JoystickPhone.jsx
+++ nuit-info/src/ui/JoystickPhone.jsx
@@ -1,9 +1,27 @@
 import { Joystick } from 'react-joystick-component';
 const JoystickPhone = () => {
+    const handleMove = (event) => {
+            console.log('Move:', event);
+        }
+
+        const handleStop = (event) => {
+            console.log('Stop:', event);
+        }
     return(
-        <div className="joystickContainer">
-    <Joystick size={100} sticky={false} baseColor="rgba(200, 200, 200, 0.7)" stickColor="rgba(255, 255, 255, 0.8)" mode="dynamic"/>
-  </div>
+            <div className="joystickContainer">
+                <Joystick 
+                    size={100} 
+                    sticky={false} 
+                    baseColor="rgba(200, 200, 200, 0.7)" 
+                    stickColor="rgba(255, 255, 255, 0.8)" 
+                    mode="dynamic"
+                    move={handleMove}
+                    stop={handleStop}
+                />
+        </div>
     )
 }
+
 export default JoystickPhone;
+
+
